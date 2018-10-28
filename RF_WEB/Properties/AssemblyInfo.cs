5f1470c586bc701c98dbc7d19ae9403c8a1b85f4
liyx <liyx0618@outlook.com>
2018-10-29 07:08:10 +0800

    WEB修改

--- RF_WEB/Properties/AssemblyInfo.cs
+++ RF_WEB/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("c16bee93-d831-44bc-a365-bb00a5843ce8")]
+[assembly: Guid("287ed411-741c-4cdd-9a92-09b2a03adc67")]
 
 // 程序集的版本信息由下列四个值组成:
 //
