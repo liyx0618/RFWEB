e9b6f046f0ff14198c0f321ae1f2c316c7c47f14
liyx <liyx0618@outlook.com>
2018-11-06 20:07:14 +0800

    提交

---
+++ RF_WEB/Controllers/PageMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RF_WEB.Controllers
+{
+    public class PageMenuController : Controller
+    {
+        // GET: PageMenu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetMenu()
+        {
+
+            return PartialView("");
+        }
+    }
+}
---
+++ RF_WEB/Models/PageMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RF_WEB.Models
+{
+    public class PageMenu
+    {
+        public string url { set; get; }
+        public string name { set; get; }
+    }
+}
